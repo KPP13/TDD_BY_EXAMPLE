9e5140c7e1917314fb5bd3de9dfa0787eef9493f
Kamil <Kamil_Poprawa@epam.com>
2016-06-25 12:32:44 +0200

    Added overriden equals() method to check equality of value object. Equals() method meets equality contract. TODO: override hashCode() method.

--- src/main/java/dollar/Dollar.java
+++ src/main/java/dollar/Dollar.java
@@ -10,4 +10,15 @@ class Dollar {
     Dollar times(int multiplier) {
         return new Dollar(amount * multiplier);
     }
+
+    // equals -> value object
+    @Override
+    public boolean equals(Object dollar) {
+        if (dollar == null) return false;
+        if (this == dollar) return true;
+        if (!this.getClass().equals( dollar.getClass() ))
+            return false;
+        Dollar dollarObject = (Dollar)dollar;
+        return amount == dollarObject.amount;
+    }
 }
--- src/test/java/dollar/DollarTest.java
+++ src/test/java/dollar/DollarTest.java
@@ -1,16 +1,31 @@
 package dollar;
 
 import org.testng.Assert;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 @Test()
 public class DollarTest {
-    @Test
-    public void testMultiplication() {
+    @Test // multiplication
+    void testMultiplication() {
         Dollar five = new Dollar(5);
         Dollar product1 = five.times(2);
         Assert.assertEquals(product1.amount, 10);
         Dollar product2 = five.times(3);
         Assert.assertEquals(product2.amount, 15);
+    }
+
+    @DataProvider(name = "equality")
+    private Object[][] provideObjectsForEquality() {
+        return new Object[][] {
+                {true, new Dollar(5), new Dollar(5)},
+                {false, new Dollar(7), new Dollar(5)},
+                {false, new Dollar(4), null}
+        };
+    }
+
+    @Test(dataProvider = "equality") // equality
+    void testEquality(boolean excepted, Dollar leftObj, Dollar rightObj) {
+        Assert.assertEquals( leftObj.equals(rightObj), excepted );
     }
 }
