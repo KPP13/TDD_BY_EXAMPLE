1566c7eb5748acebdbb30843dfa19288d935b0e3
Kamil <Kamil_Poprawa@epam.com>
2016-06-25 11:57:31 +0200

    times() changed not to modify the Dollar object. Now the method returns new object as the result of multiplying.

--- src/test/java/dollar/DollarTest.java
+++ src/test/java/dollar/DollarTest.java
@@ -5,12 +5,12 @@ import org.testng.annotations.Test;
 
 @Test()
 public class DollarTest {
-
-
     @Test
     public void testMultiplication() {
         Dollar five = new Dollar(5);
-        five.times(2);
-        Assert.assertEquals(five.amount, 10);
+        Dollar product1 = five.times(2);
+        Assert.assertEquals(product1.amount, 10);
+        Dollar product2 = five.times(3);
+        Assert.assertEquals(product2.amount, 15);
     }
 }
